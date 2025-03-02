3f4ae7d59b64e7f7c0253bc7948a19591809b8b4
Mike Myers <mikemyersco@gmail.com>
2025-03-02 07:34:27 -0700

    other

--- process-env.js
+++ process-env.js
@@ -1,4 +1,4 @@
-// This file makes environment variables available to client-side JavaScript
 window.ENV = {
-    GOOGLE_API_KEY: '{{ GOOGLE_API_KEY }}'
+    GOOGLE_API_KEY: '{{ GOOGLE_API_KEY }}',
+    SPREADSHEET_ID: '{{ SPREADSHEET_ID }}'
 };
